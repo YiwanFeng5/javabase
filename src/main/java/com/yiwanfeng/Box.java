3dc775ac76129f2c270b5d879218a0f2e0d4a0a4
fengyiwan <ily19910525fyw@126.com>
2019-08-14 14:20:16 +0800

    init

---
+++ src/main/java/com/yiwanfeng/Box.java
@@ -0,0 +1,28 @@
+package com.yiwanfeng;
+
+/***
+ * 定义一个泛型类
+ * @param <T>
+ */
+public class Box<T> {
+    private T t;
+
+    public void add(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+
+    public static void main(String[] args) {
+        Box<Integer> integerBox = new Box<>();
+        Box<String> stringBox = new Box<>();
+
+        integerBox.add(new Integer(10));
+        stringBox.add(new String("Hello"));
+
+        System.out.printf("整型值为：%d\n\n", integerBox.get());
+        System.out.printf("字符串为：%s\n", stringBox.get());
+    }
+}
--- src/test/java/com/yiwanfeng/AppTest.java
+++ src/test/java/com/yiwanfeng/AppTest.java
@@ -2,7 +2,14 @@ package com.yiwanfeng;
 
 import static org.junit.Assert.assertTrue;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidPooledConnection;
 import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Unit test for simple App.
@@ -16,5 +23,29 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void selectColumn() throws SQLException {
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            DruidDataSource dataSource = new DruidDataSource();
+            dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+            dataSource.setUsername("root");
+            dataSource.setPassword("vrv123456.");
+            dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/mysql");
+            Connection connection = dataSource.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select * from user limit 10;");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("user"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
